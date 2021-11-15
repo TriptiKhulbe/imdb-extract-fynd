5732547b6d74f227fc3fb8fa805e1e54f411298b
TriptiKhulbe <44005208+TriptiKhulbe@users.noreply.github.com>
2021-11-16 01:55:14 +0530

    postgres dialect replaced with postgresql

--- config/config.heroku.py
+++ config/config.heroku.py
@@ -7,6 +7,11 @@ ENV = "development"
 DEBUG = True
 SECRET_KEY = "adfghytr43wfvdsfbgfnhrsthergsd"
 SQLALCHEMY_ECHO = True
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+uri = os.environ['DATABASE_URL']
+if uri.startswith("postgres://"):
+  uri = uri.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URI = uri
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECURITY_PASSWORD_SALT = ""
